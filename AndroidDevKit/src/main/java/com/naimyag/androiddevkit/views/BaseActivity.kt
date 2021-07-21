0940b2c1e61f7c4328a849402866ca5dd64e9ecc
naim yag <naim@smartcozum.com>
2021-07-21 14:46:34 +0300

    - viewbinding enabled

--- AndroidDevKit/src/main/java/com/naimyag/androiddevkit/views/BaseActivity.kt
+++ AndroidDevKit/src/main/java/com/naimyag/androiddevkit/views/BaseActivity.kt
@@ -1,18 +1,25 @@
 package com.naimyag.androiddevkit.views
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
 import com.naimyag.androiddevkit.utils.IDialog
 import com.naimyag.androiddevkit.utils.ILog
 import com.naimyag.androiddevkit.utils.IToast
 
-abstract class BaseActivity : AppCompatActivity(), IToast, IDialog, ILog, IContentView {
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity(), IToast, IDialog, ILog {
+
+    protected lateinit var binding : VB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         printLog("->onCreate()")
-        setContentView(getContentView())
+        binding = inflateLayout(layoutInflater)
+        setContentView(binding.root)
     }
+
+    abstract fun inflateLayout(layoutInflater: LayoutInflater) : VB
 
     override fun onResume() {
         super.onResume()
--- app/src/main/java/com/naimyag/devkit/MainActivity.kt
+++ app/src/main/java/com/naimyag/devkit/MainActivity.kt
@@ -2,13 +2,15 @@ package com.naimyag.devkit
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.naimyag.androiddevkit.api.ApiCall
 import com.naimyag.androiddevkit.api.Result
 import com.naimyag.androiddevkit.views.BaseActivity
+import com.naimyag.devkit.databinding.ActivityMainBinding
 import com.naimyag.devkit.model.Besin
 import java.lang.Thread.sleep
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +33,21 @@ class MainActivity : BaseActivity() {
                         besinList.forEach { printLog("besin: $it") }
                     }
                     is Result.Error -> {
-                        when (resp.exception.response()!!.code()){
+                        when (resp.exception.response()!!.code()) {
                             401 -> {
                                 printEx("onCreate() getBesinler 401 ex:", resp.exception)
                                 //for example
                                 printEx("onCreate() getBesinler session closed by server, user logout!")
                             }
-                            402 -> {printEx("onCreate() getBesinler 402 ex:", resp.exception)}
-                            403 -> {printEx("onCreate() getBesinler 403 ex:", resp.exception)}
-                            404 -> {printEx("onCreate() getBesinler 404 ex:", resp.exception)}
+                            402 -> {
+                                printEx("onCreate() getBesinler 402 ex:", resp.exception)
+                            }
+                            403 -> {
+                                printEx("onCreate() getBesinler 403 ex:", resp.exception)
+                            }
+                            404 -> {
+                                printEx("onCreate() getBesinler 404 ex:", resp.exception)
+                            }
                             else -> {
                                 printEx("onCreate() getBesinler ex:", resp.exception)
                             }
@@ -58,8 +66,6 @@ class MainActivity : BaseActivity() {
         return this
     }
 
-    override fun getContentView(): Int {
-        return R.layout.activity_main
-    }
-
+    override fun inflateLayout(layoutInflater: LayoutInflater) =
+        ActivityMainBinding.inflate(layoutInflater)
 }
